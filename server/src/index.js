a8fc7b48b82c5bf987d8fc6b9b9f957571ce73fb
Harikiran Baddireddy <420111@student.nitandhra.ac.in>
2024-11-30 01:01:05 +0530

    Moved API keys to .env and updated backend configuration

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,15 @@ import cors from "cors";
 import { StreamChat } from "stream-chat";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const api_key = "w6j65gfn5sfu";
-const api_secret =
-  "9jbxttsbbghztpdnq482e4w8apgq7nvm33veuz8mwcg7wwzkvj6qrsg3scu48y4r";
+dotenv.config();
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
 const serverClient = StreamChat.getInstance(api_key, api_secret);
 
 app.post("/signup", async (req, res) => {
